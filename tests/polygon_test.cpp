04d75bf7e13230ffd7a1249fda47144eb7625f20
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-04-22 12:24:04 +0200

    Implements method to check if a point is contained in a polygon
    Inspiration:
    - http://geomalgorithms.com/a03-_inclusion.html
    - https://rosettacode.org/wiki/Ray-casting_algorithm

---
+++ tests/polygon_test.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include "../datastructures/polygon.h"
+
+TEST(PolygonTest, Contains) {
+    std::vector<vec> points;
+    points.emplace_back(vec(10, 10));
+    points.emplace_back(vec(20, 10));
+    points.emplace_back(vec(20, 20));
+    points.emplace_back(vec(10, 20));
+
+    polygon poly(points);
+
+    vec point(14, 14);
+
+    EXPECT_TRUE(poly.contains(point));
+}
+
+TEST(PolygonTest, NotContains) {
+    std::vector<vec> points;
+    points.emplace_back(vec(10, 10));
+    points.emplace_back(vec(20, 10));
+    points.emplace_back(vec(20, 20));
+    points.emplace_back(vec(10, 20));
+
+    polygon poly(points);
+
+    vec point(9, 9);
+    EXPECT_FALSE(poly.contains(point));
+}
+
+TEST(PolygonTest, ContainsWhenPointOfPolygon) {
+    std::vector<vec> points;
+    points.emplace_back(vec(10, 10));
+    points.emplace_back(vec(20, 10));
+    points.emplace_back(vec(20, 20));
+    points.emplace_back(vec(10, 20));
+
+    polygon poly(points);
+
+    vec point(10, 10);
+    EXPECT_TRUE(poly.contains(point));
+}
+
+TEST(PolygonTest, ContainsWhenPointOnEdge) {
+    std::vector<vec> points;
+    points.emplace_back(vec(10, 10));
+    points.emplace_back(vec(20, 10));
+    points.emplace_back(vec(20, 20));
+    points.emplace_back(vec(10, 20));
+    polygon poly(points);
+
+    vec point(11, 10);
+    EXPECT_TRUE(poly.contains(point));
+}
