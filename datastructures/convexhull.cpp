26ec48a42394bb65374d60b94ab9c210056f2692
Sven 'xoryo' Thiessen <me@xoryo.nl>
2019-05-12 13:51:57 +0200

    Add convex hull implementation

---
+++ datastructures/convexhull.cpp
@@ -0,0 +1,75 @@
+#include "convexhull.h"
+
+int convexhull::sqDist(const vec &p1, const vec &p2) {
+    return (p1.x - p2.x)*(p1.x - p2.x) +
+        (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+/**
+ * Check whenether the points crosses the convex hull.
+ * TODO: Add enums
+ */
+int convexhull::orientation(const vec &a, const vec &b, const vec &c) {
+    int res = (b.y - a.y) * (c.x - b.x) - (c.y - b.y) * (b.x - a.x);
+
+    if (res == 0) {
+        return 0;
+    }
+
+    return res > 0 ? 1 : -1;
+}
+
+void convexhull::add(vec &p) {
+    /*
+     * Check if the point is inside the convex hull.
+     * This can be faster if needed.
+     */
+    if (this->convex.contains(p)) {
+        return;
+    }
+
+    // point having minimum distance from the point p
+    int ind = 0;
+    std::vector<vec> points = this->convex.getPoints();
+    int n = points.size();
+    for (int i = 0; i < n; i++) {
+        if (this->sqDist(p, points[i]) < sqDist(p, points[ind])) {
+            ind = i;
+        }
+    }
+
+    /* find upper tangent. */
+    int up = ind;
+    while(orientation(p, points[up], points[(up+1)%n]) >= 0) {
+        up = (up+1) %n;
+    }
+
+    /* find lowest tangent. */
+    int low = ind;
+    while (orientation(p, points[low], points[(n+low-1)%n]) <= 0) {
+        low = (n+low - 1) % n;
+    }
+
+    std::vector<vec> result;
+    
+    /**
+     * Making the final hull by traversing points
+     * from up to low given convex hull.
+     */
+    int cur = up;
+    result.push_back(points[cur]);
+
+    while (cur != low) {
+        cur = (cur+1) %n;
+        result.push_back(points[cur]);
+    }
+
+    result.push_back(p);
+    points.clear();
+
+    for (int i = 0; i < result.size(); i++) {
+        points.push_back(result[i]);
+    }
+
+    this->convex.setPoints(points);
+}
---
+++ datastructures/convexhull.h
@@ -0,0 +1,31 @@
+/**
+ * Based on https://www.geeksforgeeks.org/dynamic-convex-hull-adding-points-existing-convex-hull/
+ */
+
+#ifndef GEOMETRY_CONTEST_CONVEXHULL_H
+#define GEOMETRY_CONTEST_CONVEXHULL_H
+
+#include "polygon.h"
+#include <vector>
+#include "vec.h"
+
+class convexhull{
+private:
+    polygon convex;
+
+    int sqDist(const vec &p1, const vec &p2);
+    int orientation(const vec &a, const vec &b, const vec &c);
+public:
+    convexhull(const polygon &poly) {
+        this->convex = poly;
+    }
+
+    convexhull(std::vector<vec> &points) {
+        this->convex = polygon();
+        this->convex.setPoints(points);
+    }
+
+    void add(vec &p);
+};
+
+#endif // GEOMETRY_CONTEST_CONVEXHULL_H
